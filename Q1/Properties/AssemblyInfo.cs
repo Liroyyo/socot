0e13089233c09b71a7fd6bca19c6341a60864e0e
Liroyyo <liroygaming1209@gmail.com>
2023-09-23 02:30:26 +0300

    uno

--- Q1/Properties/AssemblyInfo.cs
+++ Q1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("socot")]
+[assembly: AssemblyTitle("Q1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("socot")]
+[assembly: AssemblyProduct("Q1")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e00bf342-8a75-4007-a7af-f28478456b14")]
+[assembly: Guid("535f6383-0287-4b27-8b63-75680cce6d79")]
 
 // Version information for an assembly consists of the following four values:
 //
