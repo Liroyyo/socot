d4e46ea87eca4f4d591d8fcf98fe0a6754774900
Liroyyo <liroygaming1209@gmail.com>
2023-09-23 02:32:41 +0300

    added

--- Q1/Q1.cs
+++ Q1/Q1.cs
@@ -10,6 +10,26 @@ namespace Q1
     {
         static void Main(string[] args)
         {
+            int x, y, z, l, a;
+            Console.WriteLine("Enter the change amount");
+            x = int.Parse(Console.ReadLine());
+            if (x < 0)
+            {
+                Console.WriteLine("The amount can’t be negative");
+            }
+
+
+
+            else
+            {
+
+                y = x / 10;
+                z = (x - y * 10) / 5;
+                l = (x - y * 10 - z * 5) / 1;
+                a = y + z + l;
+                Console.WriteLine($"10 = {y} 5 = {z} 1 = {l} amount of coins = {a}");
+            }
+
         }
     }
 }
