94d9b8bc8a33681172afaf292c5789c172c753f3
liroy <liroy@LIROY-PC>
2023-09-23 02:23:57 +0300

    Add project files.

---
+++ Q2/Q2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Q2
+    {
+        static void Main(string[] args)
+        {
+            int x, Hundreds, Tens, Ones, Hundreds2, Tens2, Ones2, answer;
+
+            Random rnd = new Random();
+
+            x = rnd.Next(100, 1000);
+
+            Console.WriteLine(x);
+
+            Hundreds = x / 100 * 100;
+            Tens = (x - Hundreds) / 10 * 10;
+            Ones = x - Tens - Hundreds;
+
+            Hundreds2 = x / 100; ;
+            Ones2 = (x - Tens - Hundreds) * 100;
+
+            answer = Hundreds2 + Tens + Ones2;
+
+            Console.WriteLine($"Hundred = {Hundreds}");
+            Console.WriteLine($"Ten = {Tens}");
+            Console.WriteLine($"One = {Ones}");
+            Console.WriteLine(answer);
+
+        }
+    }
+}
---
+++ socot/Q1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace socot
+{
+    internal class Q1
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z, l, a;
+
+            Console.WriteLine("Enter the change amount");
+            x = int.Parse(Console.ReadLine());
+
+            if (x < 0)
+            {
+                Console.WriteLine("The amount can’t be negative");
+            }
+
+            else
+            {
+
+                y = x / 10;
+                z = (x - y * 10) / 5;
+                l = (x - y * 10 - z * 5) / 1;
+                a = y + z + l;
+                Console.WriteLine($"10 = {y} 5 = {z} 1 = {l} amount of coins = {a}");
+            }
+
+        }
+    }
+}
